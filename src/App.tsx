c514abc0b58992a71cdb9710c790dcb11853c2f5
FarhanFachrezzi <you@example.com>
2024-09-05 14:26:19 +0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Technologies from "./components/Technologies";
 function App() {
-  const [count, setCount] = useState("ya");
-
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+    <div className="w-full overflow-hidden">
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
 
-      <h1 className=""></h1>
+      <Navbar />
+      <Hero />
+      <About />
+      <Technologies />
+      <Projects />
+      <Footer />
     </div>
   );
 }
